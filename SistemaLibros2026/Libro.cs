ec5e9b002c89f63d401f19297463a03bc64f0c72
DebbieCan <dvcanelas03@gmail.com>
2026-05-14 21:05:32 -0500

    Primer commit Libro y Persona

---
+++ SistemaLibros2026/Libro.cs
@@ -0,0 +1,14 @@
+﻿public class Libro
+{
+    public string? Titulo { get; set; }
+    public string? Autor { get; set; }
+    public int AñoPublicacion { get; set; }
+
+    public void MostrarInformacion()
+    {
+        Console.WriteLine($"Título: {Titulo}");
+        Console.WriteLine($"Autor: {Autor}");
+        Console.WriteLine($"Año de Publicación: {AñoPublicacion}");
+        Console.WriteLine();
+    }
+}
